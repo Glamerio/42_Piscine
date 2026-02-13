f157febf00357975a1e0bf7c2fb06ff45691a3ee
glamerio <halilersoy804@gmail.com>
2026-02-13 22:35:09 +0300

    C00/ex02: ft_print_reverse_alphabet added

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glamerio <glamerio@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 22:30:40 by glamerio          #+#    #+#             */
+/*   Updated: 2026/02/13 22:34:20 by glamerio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+		char	letter;
+
+		letter = 'z';
+		while (letter >= 'a')
+		{
+				write(1, &letter, 1);
+				letter--;
+		}
+}
