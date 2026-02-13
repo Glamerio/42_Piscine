b5e234176c1b73b9011bd0a6715ad3f0daaaa6e0
glamerio <halilersoy804@gmail.com>
2026-02-13 22:28:47 +0300

    C00/ex01: ft_print_alphabet added

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: glamerio <glamerio@student.42istanbul.com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/13 22:18:28 by glamerio          #+#    #+#             */
-/*   Updated: 2026/02/13 22:21:14 by glamerio         ###   ########.fr       */
+/*   Created: 2026/02/13 22:17:42 by glamerio          #+#    #+#             */
+/*   Updated: 2026/02/13 22:27:19 by glamerio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_print_alphabet(void)
 {
-	write(1, &c, 1);
+		char	letter;
+		
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			write(1, &letter, 1);
+			letter++;
+		}
 }
