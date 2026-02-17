864e75b028f4e8f68722792a10e3c2f8b1bd2029
glamerio <halilersoy804@gmail.com>
2026-02-17 17:19:54 +0300

    Add ft_strcapitalize

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glamerio <glamerio@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 16:42:46 by glamerio          #+#    #+#             */
+/*   Updated: 2026/02/17 17:14:53 by glamerio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	ft_strlowcase(str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if
+			(i == 0 || !((str[i - 1] >= 'a' && str[i - 1] <= 'z')
+				|| (str[i - 1] >= 'A' && str[i - 1] <= 'Z')
+				|| (str[i - 1] >= '0' && str[i - 1] <= '9')
+				)
+			)
+				str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
